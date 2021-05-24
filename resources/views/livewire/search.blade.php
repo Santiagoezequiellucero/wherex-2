929f21e8c9a3d9da2bd701eb74fc41536555f438
Santiago <santiagoezequiellucero@gmail.com>
2021-05-24 13:08:49 -0400

    13:08

---
+++ resources/views/livewire/search.blade.php
@@ -0,0 +1,7 @@
+<div class="flex-1 relative px-4">
+    <x-jet-input type="text" class="w-full" placeholder="Buscar" />
+
+    <button class="absolute top-0 right-0 w-12 h-full bg-blue-600 flex items-center justify-center rounded-r-md">
+        <x-search size="35" color="white"/>
+    </button>
+</div>
