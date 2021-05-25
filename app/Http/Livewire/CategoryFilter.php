7a7c1d4e6a17b3a1d1ce7ce2dd098cc0d8c0abd7
Santiago <santiagoezequiellucero@gmail.com>
2021-05-25 19:47:04 -0400

    25

--- app/Http/Livewire/CategoryFilter.php
+++ app/Http/Livewire/CategoryFilter.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,13 +12,25 @@ class CategoryFilter extends Component
 
     use WithPagination;
 
-    public $category;
+    public $category, $subcategoria, $marca;
+
+    public function limpiar(){
+        $this->reset(['subcategoria', 'marca']);
+    }
 
     public function render()
     {
+        $productsQuery = Product::query()->whereHas('subcategory.category', function(Builder $query){
+            $query->where('id', $this->category->id);
+        });
 
-        $products = $this->category->products()
-                    ->where('status', 2)->paginate(20);
+        if($this->subcategoria){
+            $productsQuery = $productsQuery->whereHas('subcategory', function(Builder $query){
+                $query->where('name', $this->subcategoria);
+            });
+        }
+
+        $products = $productsQuery->paginate(20);
 
         return view('livewire.category-filter', compact('products'));
     }
--- resources/views/livewire/category-filter.blade.php
+++ resources/views/livewire/category-filter.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="bg-white rounded-lg shadow-lg mb-4">
+    <div class="bg-white rounded-lg shadow-lg mb-6">
         <div class="px-6 py-2 flex justify-between items-center">
             <h1 class="font-semibold text-gray-500 uppercase">{{$category->name}}</h1>
 
@@ -10,17 +10,37 @@
         </div>
     </div>
 
-    <div class="grid grid-cols-5">
+    <div class="grid grid-cols-5 gap-6">
 
         <aside>
             <h2 class="font-semibold text-center mb-2">Subcategorías</h2>
-            <ul>
+            <ul class="divide-y divide-gray-200">
                 @foreach ($category->subcategories as $subcategory)
-                    <li class="my-2 text-sm">
-                        <a class="cursor-pointer hover:text-blue-500 capitalize" href="">{{$subcategory->name}}</a>
+                    <li class="py-2 text-sm">
+                        <a 
+                        wire:click="$set('subcategoria', '{{$subcategory->name}}')"
+                        class="cursor-pointer hover:text-blue-500 capitalize {{$subcategoria == $subcategory->name ? 'text-blue-500 font-semibold' :''}}">
+                            {{$subcategory->name}}
+                        </a>
                     </li>
                 @endforeach
             </ul>
+            <h2 class="font-semibold text-center mt-4 mb-2">Marcas</h2>
+            <ul class="divide-y divide-gray-200">
+                @foreach ($category->brands as $brand)
+                    <li class="py-2 text-sm">
+                        <a 
+                        wire:click="$set('marca', '{{$brand->name}}')"
+                        class="cursor-pointer hover:text-blue-500 capitalize {{$marca == $brand->name ? 'text-blue-500 font-semibold' :''}}">
+                            {{$brand->name}}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+
+            <x-jet-button class="mt-4" wire:click="limpiar">
+                Eliminar Filtros
+            </x-jet-button>
         </aside>
 
         <div class="col-span-4">
@@ -46,6 +66,8 @@
                     </li>
                 @endforeach
             </ul>
+
+            
 
             <div class="mt-4">
                 {{$products->links()}}
